75bcf44d09372edf5b8d419c3a809003465de2b6
Richard Albert Leddy <richardaleddy@gmail.com>
2021-07-22 12:52:32 -0700

    Test basic functionality of the persistence service

--- blog-services/test/index.js
+++ blog-services/test/index.js
@@ -1,9 +1,33 @@
 
-const {PersistenceCategory} = require("categorical-handlers")
+const test = require('ava');
+
 //
 const fs = require('fs')
 const fsPromises = require('fs/promises')
 const crypto = require('crypto')
+
+
+
+// TEST SUPER CLASS
+//
+class PersistenceCategoryTest {
+    //
+    constructor(conf) {
+        this.user_directory = conf.user_directory
+        this.t_topic = false
+        this.t_pub_obj = false
+
+    }
+    //
+    add_to_topic(topic,client_name,relayer) {}
+    async app_message_handler(msg_obj) {}
+    app_publish(topic,pub_obj) {
+        this.t_topic = topic
+        this.t_pub_obj = pub_obj
+    }
+    //
+}
+
 
 // connect to a relay service...
 // set by configuration (only one connection, will have two paths.)
@@ -36,7 +60,7 @@ function map_entry_type_to_producer(entry_type) {
 
 // -- -- -- --
 
-class TransitionsPersistenceEndpoint extends PersistenceCategory {
+class TransitionsPersistenceEndpoint extends PersistenceCategoryTest {
 
     //
     constructor(conf) {
@@ -344,14 +368,52 @@ class TransitionsPersistenceEndpoint extends PersistenceCategory {
             }
         }
     }
+}
+
+
+
+class test_TransitionsPersistenceEndpoint extends TransitionsPersistenceEndpoint {
+
+    constructor(conf) {
+        super(conf)
+
+        this.entry = 
+        `{
+            "date":"2021-07-22T06:19:15.710Z",
+            "dates":{
+                "created":1626934755710,
+                "updated":1626934755710
+            },
+            "tag_line":"<button id='dashboard-tagline-maker' onclick='make_dashoard_tagline(event)'>add tag line</button>",
+            "panel_key":"dc7192b50cf4e6899365a584d3f8d7625b2d4cf85b5bdcc892ff936e42e68028",
+            "path_key":"dashboard",
+            "dashboard_text":"<button id='dashboard-text-maker' onclick='make_dashoard_text(event)'>add text</button>",
+            "entries":{}
+        }`
+    }
+
+    async get_entries(entries_file) {
+        try {
+            let entries_record =  this.entry
+            entries_record = JSON.parse(entries_record.toString())
+            entries_record.test_file = entries_file
+            return entries_record    
+        } catch (e) {}
+        return false
+    }
+
+    async put_entries(entries_file,entries_record) {
+        entries_record.test_file = entries_file
+        let entries_record_str = JSON.stringify(entries_record)         // STORE AS STRING
+        this.entry = entries_record_str
+        return entries_record_str
+    }
 
 }
 
 
 
-
-
-let conf_file = 'relay-service.conf'
+let conf_file = 'test/relay-service-test.conf'
 let conf_par = process.argv[2]
 if ( conf_par !== undefined ) {
     conf_file = conf_par
@@ -359,5 +421,166 @@ if ( conf_par !== undefined ) {
 
 let conf = JSON.parse(fs.readFileSync(conf_file).toString())
 
+let testObj = new test_TransitionsPersistenceEndpoint(conf.persistence_endpoint)
 
-new TransitionsPersistenceEndpoint(conf.persistence_endpoint)
+
+test("make a path", t => {
+    console.log("making a path")
+
+    let u_topic = testObj.topic_producer_user("peanuts","joe")
+    let what_topc = testObj.topic_producer("peanuts","joe")
+
+    t.is(u_topic,what_topc)
+
+    let s_topic = testObj.topic_producer_system("peanuts","joe")
+    t.false(s_topic === u_topic)
+
+
+    let p_obj = {
+        "you" : "are",
+        "here" : true
+    }
+    let key = testObj.app_generate_tracking(p_obj)
+
+    console.log(key)
+
+    t.is(key.indexOf('/'),-1)
+
+    let f_obj = {
+        "_transition_path" : "tester",
+        "tester" : "butter"
+    }
+    // ----
+    let f_path = testObj.make_path(f_obj)
+    t.false(f_path)
+
+    let user_id = 'jam'
+    let entry_type = 'butter'
+    let file = 'bread'
+    let u_obj = {
+        "_transition_path" : "tester",
+        "tester" : "jam+butter+bread"
+    }
+    // ----
+    let path = testObj.make_path(u_obj)
+    let pp = `${testObj.user_directory}/${file}/${entry_type}/${user_id}.json`
+
+    t.is(pp,path)
+
+    u_obj = {
+        "_transition_path" : "tester",
+        "tester" : "jam+butter"
+    }
+    // ----
+    path = testObj.make_path(u_obj)
+    pp =  `${testObj.user_directory}/${entry_type}/${user_id}.json`
+
+    t.is(pp,path)
+
+    t.pass("Made a path")
+})
+
+
+test("dates and update", t => {
+
+    let u_obj = {
+        "_transition_path" : "tester",
+        "tester" : "jam+butter+bread"
+    }
+    // ----
+    let data = `{ "this" : "is", "a" : "test" }`
+    //
+    let out_data = testObj.application_data_update(u_obj,data)
+    out_data = JSON.parse(out_data)
+    console.dir(out_data)
+    // ----
+
+    testObj.user_manage_date('C',u_obj)
+    testObj.user_manage_date('U',u_obj)
+    console.dir(u_obj)
+
+    //
+    t.pass("dates and update")
+})
+
+
+// ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+
+// ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+//
+test("entry record", async t => {
+
+    let t_entries_file = "just_a_test.json"
+    let t_entries_record = await testObj.get_entries(t_entries_file)
+    console.dir(t_entries_record)
+    let entries_str = await testObj.put_entries(t_entries_file,t_entries_record)
+    t_entries_record = await testObj.get_entries(t_entries_file)
+
+    t.is(JSON.stringify(t_entries_record),entries_str)
+
+    let user_id = "anyone"
+    let user_path = `/${user_id}`
+    let entry_type = "blog"
+
+    let [entries_record, producer_of_type, entries_file] = await testObj.get_entries_record(user_path,entry_type)
+
+    console.dir(entries_record)
+    console.log(producer_of_type)
+    console.log(entries_file)
+
+    await testObj.write_entry_file(entries_file,entries_record,producer_of_type,user_id)
+
+    console.log("after write_entry_file")
+    console.log(testObj.t_topic)
+    testObj.t_pub_obj.dashboard = decodeURIComponent(testObj.t_pub_obj.dashboard)
+    console.dir(testObj.t_pub_obj)
+    //
+    let entry_obj_1 = {
+        "_id" : 1,
+        "title" : "this is a thing with a title"
+    }
+    testObj.create_entry_type(entry_obj_1,user_path,entries_record,entry_type)
+    t.is(entries_record.entries[entry_type].length,1)
+    let entry_obj_2 = {
+        "_id" : 2,
+        "title" : "this is a thing with a title, too"
+    }
+    testObj.create_entry_type(entry_obj_2,user_path,entries_record,entry_type)
+    t.is(entries_record.entries[entry_type].length,2)
+    //
+    console.dir(entries_record.entries[entry_type])
+    let entry_obj_3 = {
+        "_id" : 3,
+        "title" : "this is a thing with a title, three"
+    }
+    testObj.create_entry_type(entry_obj_3,user_path,entries_record,entry_type)
+    t.is(entries_record.entries[entry_type].length,3)
+    //
+    entry_obj_2 = {
+        "_id" : 2,
+        "title" : "this is a thing with a title, too",
+        "added" : "this is added"
+    }
+    testObj.update_entry_type(entry_obj_2,user_path,entries_record,entry_type)
+    console.dir(entries_record.entries[entry_type])
+    t.is(entries_record.entries[entry_type][1].added,"this is added")
+    //
+    let field = "added"
+    entry_obj_2.added = "THIS IS TOTALLY CHANGED"
+    testObj.update_entry_type_field(entry_obj_2,user_path,entries_record,entry_type,field)
+    console.dir(entries_record.entries[entry_type])
+    t.is(entries_record.entries[entry_type][1].added, "THIS IS TOTALLY CHANGED")
+
+    field = "added"
+    entry_obj_3.added = "AND THIS WAS LEFT BEHIND"
+    testObj.update_entry_type_field(entry_obj_3,user_path,entries_record,entry_type,field)
+    console.dir(entries_record.entries[entry_type])
+    t.is(entries_record.entries[entry_type][2].added, "AND THIS WAS LEFT BEHIND")
+    //
+
+    testObj.delete_entry_type(entry_obj_2,entries_record,entry_type)
+    t.is(entries_record.entries[entry_type][1].added, "AND THIS WAS LEFT BEHIND")
+
+    //
+    t.pass("entry record")
+})
