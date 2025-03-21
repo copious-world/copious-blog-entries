ef2d94c008632cd57baf28084e86173970f23346
Richard Albert Leddy <858872+rleddy@users.noreply.github.com>
2025-03-21 12:38:53 -0700

    Some cleanup
    Also some more versatility.

--- blog-services/endpoints/user_category_server.js
+++ blog-services/endpoints/user_category_server.js
@@ -1,8 +1,7 @@
 //
 const {UserCategory} = require("categorical-handlers")
-const user_dashboard_generator = require("../transitions/dashboard").generator
-const user_profile_generator = require("../transitions/profile").generator
-
+//
+let g_loaded_targets = false
 //
 const fs = require('fs')
 const crypto = require('crypto')
@@ -14,15 +13,6 @@ if ( conf_par !== undefined ) {
 }
 
 let conf = JSON.parse(fs.readFileSync(conf_file).toString())
-
-//console.dir(conf)
-
-// _gen_targets is used each time a new user is created...
-conf.user_endpoint._gen_targets = {
-    "profile" : user_profile_generator,         // generator function...
-    "dashboard" : user_dashboard_generator     // generator function...
-}
-
 
 //  app_generate_tracking(msg_obj)
 
@@ -42,6 +32,7 @@ class TransitionsUserEndpoint extends UserCategory {
         super(conf)
         this.user_tracking_table = {}        // keep track of all the user registration files.
         this.new_tracking = {}
+        this.gen_targets = g_loaded_targets
     }
 
     app_generate_tracking(u_obj) {
@@ -50,14 +41,23 @@ class TransitionsUserEndpoint extends UserCategory {
         return(do_hash(uobj_str))
     }
 
-    app_asset_generator(u_obj,gen_targets) {
+
+    /**
+     * app_asset_generator
+     * 
+     * output assets map which is a key to a JSON object 
+     * 
+     * @param {object} u_obj 
+     * @returns object 
+     */
+    app_asset_generator(u_obj) {
         let storables = {}
-        if ( gen_targets ) {
-            for ( let target in gen_targets ) {
-                let generator = gen_targets[target]
+        if ( this.gen_targets ) {
+            for ( let target in this.gen_targets ) {
+                let generator = this.gen_targets[target]
                 let output = generator(u_obj,target)
                 storables[target] = output
-            }    
+            }
         }
         return storables
     }
@@ -86,6 +86,12 @@ class TransitionsUserEndpoint extends UserCategory {
         return(user_path)
     }
 
+
+    /**
+     * _startup_track_users_from_directory
+     * 
+     * prevent duplicates
+     */
     _startup_track_users_from_directory() {
         let au = this.all_users.trim()
         let dir_list = fs.readdirSync(au)
@@ -117,8 +123,31 @@ class TransitionsUserEndpoint extends UserCategory {
 }
 
 
+
+
+function load_generators() {
+    //
+    let gen_dir = conf.generator_directory ? conf.generator_directory : "../generators"
+    //
+    try {
+        g_loaded_targets = {}
+        let gen_files = fs.readdirSync(gen_dir)
+        for ( let file of gen_files ) {
+            try {
+                let gen_func = require(file).generator
+                g_loaded_targets[file] = gen_func    
+            } catch (e) {}
+        }    
+    } catch (e) {}
+    //
+}
+
+
 console.log(`User Server: PORT: ${conf.user_endpoint.port} ADDRESS: ${conf.user_endpoint.address}`)
 
+load_generators()
+//
 let tue = new TransitionsUserEndpoint(conf.user_endpoint)
 
+// prevent duplicates
 tue._startup_track_users_from_directory()
